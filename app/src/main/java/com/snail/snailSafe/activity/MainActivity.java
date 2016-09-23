5a1f532f4e9be6d7da3a9ee952686b565d6e14ed
snail <snail>
2016-09-23 10:09:37 +0800

    工程结构搭建

--- app/src/main/java/com/snail/snailSafe/activity/MainActivity.java
+++ app/src/main/java/com/snail/snailSafe/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.snail.snailSafe;
+package com.snail.snailSafe.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.snail.snailSafe.R;
 
 public class MainActivity extends AppCompatActivity {
 
